35a3ddd38d29793b300bc8dbe12dc28841988d82
wasabina67 <wasabina67@gmail.com>
2025-01-02 11:32:38 +0900

    Update run.py

--- run.py
+++ run.py
@@ -9,16 +9,17 @@ def func_10():
     print(f"-- {f_name} end")
 
 
-def func_2():
+def func_2(t_10):
     f_name = func_2.__name__
     print(f"-- {f_name} begin")
     time.sleep(2)
+    t_10.join()
     print(f"-- {f_name} end")
 
 
 def main():
     t_10 = threading.Thread(target=func_10, name="t_10")
-    t_2 = threading.Thread(target=func_2, name="t_2")
+    t_2 = threading.Thread(target=func_2, args=(t_10,), name="t_2")
 
     t_10.start()
     t_2.start()
