09be8fdac9a3d477508b217b46db45a07658e7db
wasabina67 <wasabina67@gmail.com>
2025-01-02 11:24:58 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,20 @@
+import threading
+import time
+
+
+def func_10():
+    f_name = func_10.__name__
+    print(f"-- {f_name} begin")
+
+
+def func_2():
+    f_name = func_2.__name__
+    print(f"-- {f_name} begin")
+
+
 def main():
-    pass
+    func_10()
+    func_2()
 
 
 if __name__ == "__main__":
